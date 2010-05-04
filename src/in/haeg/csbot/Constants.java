57118c26bb7f4a71b4871e1136903ae067684df8
Harry Mills <harry@haeg.in>
2010-05-04 23:44:52 +0100

    Initial commit.
    Connects to the server and handles karma. Also corrects prettygreat's
    part message.

---
+++ src/in/haeg/csbot/Constants.java
@@ -0,0 +1,11 @@
+package in.haeg.csbot;
+
+public final class Constants {
+    public static final String HOST      = "irc.freenode.net";
+    public static final int[]  PORT      = { 6667, };
+    public static final String PASSWORD  = "";
+    public static final String NICK_NAME = "CSYorkBot";
+    public static final String REAL_NAME = "Haegin's CS Bot";
+    public static final String USER_NAME = "Haegins CS Bot";
+    public static final String CHANNEL   = "#cs-york";
+}
